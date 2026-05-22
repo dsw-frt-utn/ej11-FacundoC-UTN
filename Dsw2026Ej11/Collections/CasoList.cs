b728b028928be02d468de77d3269dadf0d92ba26
FacundoC-UTN <FacundoLeonel.Castro@alu.frt.utn.edu.ar>
2026-05-22 17:32:05 -0300

    CasoLIST, metodos agregados

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,11 +1,54 @@
 ﻿namespace Dsw2026Ej11.Collections;
+using System.Linq; 
+using Dsw2026Ej11.Domain;
 
-//Crear un campo que represente una lista de alumnos (List<>)
-//Incluir un método para agregar alumnos a la lista
-//Incluir un método para retornar la lista
-//Incluir un método para buscar un alumno por nombre
-//Incluir un método para eliminar un alumno (debe recibir un alumno)
-//Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    // Lista de alumnos
+    private List<Alumno> _alumnos;
+
+    // Constructor con lista vacia
+    public CasoList()
+    {
+        _alumnos = new List<Alumno>();
+    }
+
+    // Agregar alumnos a la lista
+    public void AgregarAlumno(Alumno alumno)
+    {
+        
+        _alumnos.Add(alumno);
+    }
+
+    // Retoma la lista
+    public List<Alumno> ObtenerLista()
+    {
+        return _alumnos;
+    }
+
+    // Busqueda de alumno por nombre
+    public Alumno BuscarPorNombre(string nombre)
+    {
+        // .FirstOrDefault() busca el primero que coincida con el nombre.
+        // No importa el metodo de escritura(mayus o minus) lo va encontrar igual
+        return _alumnos.FirstOrDefault(a => a.Nombre.Equals(nombre, System.StringComparison.OrdinalIgnoreCase));
+    }
+
+    // Eliminar el alumno
+    public void EliminarAlumno(Alumno alumno)
+    {
+       
+        _alumnos.Remove(alumno);
+    }
+
+    // Elimina el alumno en una posicion especifica de la lista
+    public void EliminarEnPosicion(int posicion)
+    {
+        // Verificacion del numero ingresado
+        if (posicion >= 0 && posicion < _alumnos.Count)
+        {
+            //Remueve alumno en la posicion teniendo en cuenta que arranca contando desde 0
+            _alumnos.RemoveAt(posicion);
+        }
+    }
 }
